a6cfb8acf5a9fd5b9dd57cfbe6e9d87330752ccc
aryanuu21 <aryanpratapsingh674@gmail.com>
2025-08-21 17:43:53 +0530

    changes made

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,17 @@
-import React from 'react'
+"use client"
+import React, { useState } from 'react'
 
 const page = () => {
+  const [name, setname] = useState<string>("")
   return (
-    <div>page</div>
+      <>
+        <form action="">
+          <input value={name} onChange={e=>setname(e.target.value)} type="text" name='name' placeholder='Enter your name' /> <br />
+          <button type='submit'>
+            get info
+          </button>
+        </form>
+      </>
   )
 }
 
