280156e4669fa9afd496278b9561092dc8df06bd
aryanuu21 <aryanpratapsingh674@gmail.com>
2025-08-21 18:11:13 +0530

    connected to api

--- app/info/[name]/page.tsx
+++ app/info/[name]/page.tsx
@@ -1,13 +1,39 @@
 import React from 'react';
 
+const getPredAge = async(name:string)=>{
+    const res  = await fetch (`https://api.agify.io?name=${name}`);
+    if (!res.ok) {
+        throw new Error('Failed to fetch age');
+    }
+    return res.json();
+}
+
+const getPredGender = async (name:string)=>{
+    const res = await fetch(`https://api.genderize.io?name=${name}`);
+    if (!res.ok) {
+        throw new Error('Failed to fetch age');
+    }
+    return res.json();
+}
+
+const getPredCountry = async (name:string)=>{
+    const res = await fetch(`https://api.nationalize.io?name=${name}`);
+    if (!res.ok) {
+        throw new Error('Failed to fetch age');
+    }
+    return res.json();
+}
+
 interface PageProps {
     params: {
         name: string;
     };
 }
 
-const InfoPage: React.FC<PageProps> = ({ params }) => {
-
+const InfoPage: React.FC<PageProps> = async ({ params }) => {
+    const ageData = await getPredAge(params.name);
+    const genderData = await getPredGender(params.name);
+    const countryData = await getPredCountry(params.name);
 
     return (
         <div>
